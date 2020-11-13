28ea54f71c9d74cd3000d4e2388a387504dd4af2
Soohwan Lee <soohwan.lee@cern.ch>
2020-11-14 02:40:50 +0900

    fix

--- doMassFit.sh
+++ doMassFit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-root -l -b -q 'MassYieldFit.C(0, 50, -2.4, 2.4, "3p5", 1, 90)'
-root -l -b -q 'MassYieldFit.C(0, 50, -2.4, 2.4, "3p5", 2, 90)'
-root -l -b -q 'MassYieldFit.C(0, 50, -2.4, 2.4, "4", 1, 90)'
-root -l -b -q 'MassYieldFit.C(0, 50, -2.4, 2.4, "4", 2, 90)'
+root -l -b -q 'MassYieldFit.C(0, 50, -2.4, 2.4, "3p5", 1, 180)'
+root -l -b -q 'MassYieldFit.C(0, 50, -2.4, 2.4, "3p5", 2, 180)'
+root -l -b -q 'MassYieldFit.C(0, 50, -2.4, 2.4, "4", 1, 180)'
+root -l -b -q 'MassYieldFit.C(0, 50, -2.4, 2.4, "4", 2, 180)'
