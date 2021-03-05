8e703f328d2b3a0e8c61f4568e4c6d31bc3853b6
Soohwan Lee <soohwan.lee@cern.ch>
2021-03-05 17:05:54 +0900

    update

---
+++ BDT/doFit_BDT_MC.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ts=$1
+export timestamp=$ts
+	cd ..
+root -l -b -q 'MassYieldFit_BDT_MC.C('$timestamp',"", "OniaRooDataset_BDT'$timestamp'_OniaSkim_TrigS13_BDT_MC.root", 0, 30, -2.4, 2.4, "3p5", "S13", 180, 3, false, 0.01, -1.0 ,1.)' &  2>/dev/null
